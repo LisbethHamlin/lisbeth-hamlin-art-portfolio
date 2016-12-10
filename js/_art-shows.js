b71ad6b168762ca1001c190e2b7c6fd93e415a06
Bryce Mehring <brycemehring@gmail.com>
2016-12-10 17:56:18 -0600

    Added webpack js files. Removed homepage js. Cleaned up art show posts. Show art-show excerpt on homepage.

--- js/_art-shows.js
+++ js/_art-shows.js
@@ -1,25 +1,17 @@
 'use strict';
 
-require('./_common.js');
+var common = require('./_common.js');
+var $target = $('#previous-shows');
 
-(function($) {
-  var $showRoot = $('#show-root');
-
-  if(!$showRoot) {
-    return;
+common.verifyItemTime($, $('#current-shows > div'), function($element, showEnded) {
+  if(showEnded) {
+    $element.detach();
+    $target.prepend($element);
   }
+});
 
-  var $target = $('#previous-shows');
-  verifyItemTime($, $('#current-shows > div'), function($element, showEnded) {
-    if(showEnded) {
-      $element.detach();
-      $target.prepend($element);
-    }
-  });
-
-  if($('#current-shows > div').length === 0) {
-    $('#current-shows').remove();
-    $('#no-current-shows-message').removeClass('hidden');
-  }
-  $showRoot.removeClass('load');
-})(jQuery);
+if($('#current-shows > div').length === 0) {
+  $('#current-shows').remove();
+  $('#no-current-shows-message').removeClass('hidden');
+}
+$('#show-root').removeClass('load');
