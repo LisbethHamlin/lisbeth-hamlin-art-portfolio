f6cb805218b973ba0ae8b01d53ba9731eef160e8
Bryce Mehring <brycemehring@gmail.com>
2017-01-22 22:51:10 -0600

    Optimized art show page.

--- js/_art-shows.js
+++ js/_art-shows.js
@@ -1,16 +1,15 @@
 'use strict';
 
 var common = require('./_common.js');
-var $target = $('#previous-shows');
-
-common.verifyItemTime($, $('#current-shows > div'), function($element, showEnded) {
-  if(showEnded) {
+var $target = $('#current-shows');
+common.verifyItemTime($, $('#previous-shows > div'), function($element, showEnded) {
+  if(!showEnded) {
     $element.detach();
     $target.prepend($element);
   }
 });
 
-if($('#current-shows > div').length === 0) {
+if(!$target.is(':parent')) {
   $('#current-shows').remove();
   $('#no-current-shows-message').removeClass('hidden');
 }
