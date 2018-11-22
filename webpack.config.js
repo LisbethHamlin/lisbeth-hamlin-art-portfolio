91bf97c213110a0e6ef121467e015e0e36b045a8
Bryce Mehring <brycemehring@gmail.com>
2018-11-21 21:06:59 -0600

    Upgraded packages

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
   module: {
     rules: [{
         test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
