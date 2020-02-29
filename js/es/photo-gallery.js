5bc4236275d27c832b54416f5dd724ac547affaf
Bryce Mehring <brycemehring@gmail.com>
2020-02-29 11:10:19 -0600

    Use slim version of jquery

--- js/es/photo-gallery.js
+++ js/es/photo-gallery.js
@@ -1,4 +1,4 @@
-import{c as e,a as n,$ as t}from"./jquery.js";var o=e((function(e,n){
+import{c as e,a as n,$ as t}from"./jquery.slim.js";var o=e((function(e,n){
 /*! PhotoSwipe - v4.1.3 - 2019-01-08
 * http://photoswipe.com
 * Copyright (c) 2019 Dmitry Semenov; */
--- js/photo-gallery.js
+++ js/photo-gallery.js
@@ -1,4 +1,4 @@
-import $ from 'jquery';
+import $ from 'jquery/dist/jquery.slim';
 import photoswipe from 'photoswipe';
 import photoswipeUI from 'photoswipe/dist/photoswipe-ui-default';
 
