9b5074812723c60a883076e8898b51988ed91572
Bryce Mehring <brycemehring@gmail.com>
2020-09-12 23:10:15 -0500

    Upgrade packages Fixed rollup warnings

--- rollup.config.js
+++ rollup.config.js
@@ -14,6 +14,7 @@ export default {
     chunkFileNames: `chunk-[name]-[hash].js`,
     sourcemap: true,
   },
+  context: 'window',
   preserveEntrySignatures: false,
   plugins: [
     resolve({ browser: true }),
