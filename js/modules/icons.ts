14da31a650e4ccd1e2f50b2a02aa17fc05f07582
Bryce Mehring <brycemehring@gmail.com>
2021-01-23 19:12:33 -0600

    Use esm modules

--- js/modules/icons.ts
+++
@@ -1,8 +0,0 @@
-import { library, dom } from '@fortawesome/fontawesome-svg-core';
-import { faFolderOpen } from '@fortawesome/free-solid-svg-icons';
-import { faCalendarAlt } from '@fortawesome/free-regular-svg-icons';
-import { faFacebookF, faTwitter } from '@fortawesome/free-brands-svg-icons';
-
-library.add(faCalendarAlt, faFolderOpen, faFacebookF, faTwitter);
-
-dom.i2svg();
