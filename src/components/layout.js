c44eb60e8a39a3d681f315d67f6d4583db0177e6
Bryce Mehring <brycemehring@gmail.com>
2021-08-21 19:51:50 -0500

    Add favicon

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,21 +3,21 @@ import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import { Header } from './header';
 import { Footer } from './footer';
+import favicon from '../images/favicon.png';
 
 import '../styles/styles.scss';
 
 export const Layout = ({ children }) => {
   return (
-    <>
-      <div className="container lisbeth-hamlin">
-        <Helmet>
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Indie+Flower" />
-        </Helmet>
-        <Header />
-        {children}
-        <Footer />
-      </div>
-    </>
+    <div className="container lisbeth-hamlin">
+      <Helmet>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Indie+Flower" />
+        <link rel="icon" type="image/png" href={favicon} />
+      </Helmet>
+      <Header />
+      {children}
+      <Footer />
+    </div>
   );
 };
 
