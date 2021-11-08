b537621ea8f207308fab768ddaae320bea812669
Bryce Mehring <brycemehring@gmail.com>
2021-11-07 21:39:55 -0600

    Fix no contentful schema

--- src/components/useArtShows.js
+++ src/components/useArtShows.js
@@ -3,12 +3,11 @@ import { graphql, useStaticQuery } from 'gatsby';
 export const useArtShows = () => {
   return useStaticQuery(graphql`
     query {
-      artShows: allContentfulArtShows(sort: { order: ASC, fields: endDate }) {
+      artShows: allContentfulArtShows(filter: { ignore: { eq: false } }, sort: { order: ASC, fields: endDate }) {
         nodes {
           displayField
           endDate
           excerpt
-          id
         }
       }
     }
