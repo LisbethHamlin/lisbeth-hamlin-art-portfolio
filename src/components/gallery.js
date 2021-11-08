1eecbee29df9ed4e6acd97c33844e236e540fd91
Bryce Mehring <brycemehring@gmail.com>
2021-11-07 21:46:33 -0600

    Use react dom renderer instead of manually creating HTML for photoswipe

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -3,19 +3,25 @@ import { usePhotoSwipe } from './usePhotoSwipe';
 import { PortfolioImage } from './portfolioImage';
 import { cleanGroup } from '../utils';
 import { prevent } from './prevent';
+import { renderToStaticMarkup } from 'react-dom/server.browser';
 
 export const Gallery = (props) => {
   const images = props.images.map(({ image, title, description, file }) => {
     const { childImageSharp } = file;
     const { src, width, height } = childImageSharp.original;
     const displayTitle = title || cleanGroup(image);
-    let captionDescription = '';
     let alt = displayTitle;
 
     if (description) {
-      captionDescription = `<p>${description}</p>`;
       alt += ': ' + description;
     }
+
+    const caption = (
+      <>
+        <p className="text-capitalize">{displayTitle}</p>
+        {description && <p>{description}</p>}
+      </>
+    );
 
     return {
       childImageSharp: file.childImageSharp,
@@ -23,10 +29,7 @@ export const Gallery = (props) => {
       w: width,
       h: height,
       alt,
-      captionHTML: `
-          <p class="text-capitalize">${displayTitle}</p>
-          ${captionDescription}
-        `,
+      captionHTML: renderToStaticMarkup(caption),
     };
   });
 
