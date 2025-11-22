3d7c2ce5cd692abf84859f8ed05e21bf777b0de4
Bryce Mehring <brycemehring@gmail.com>
2025-11-22 09:50:03 -0600

    add font weight

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,7 +12,7 @@ export const Layout = ({ children }) => {
       <Helmet htmlAttributes={{ lang: 'en' }}>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link rel="stylesheet" href={'https://fonts.googleapis.com/css2?family=Andada+Pro&display=swap'} />
+        <link rel="stylesheet" href={'https://fonts.googleapis.com/css2?family=Andada+Pro:wght@400;700&display=swap'} />
         <link rel="icon" type="image/png" href={favicon} />
       </Helmet>
       <Header />
